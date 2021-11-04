1e55a2997c4fc08273580910cfd6f554a768eb72
MathieuWasselin <wasselinmathieu@gmail.com>
2021-11-04 13:48:07 +0100

    changement2

--- style.js
+++ style.js
@@ -11,3 +11,18 @@ for (let i = 0; i < test; i++) {
 document.getElementById("p").addEventListener("click");
 
  }
+ 
+
+ //function listeChiffre() {
+// Donne la valeur de l'input à la variable numero
+let numero=document.getElementById("id1").value;
+// boucle for, variable i va de 0 à la valeur de l'input, incrémentation ++ = + 1
+for (let i = 0; i<=document.getElementById("id1").value; i++)
+{
+    // instruction pour que la boucle s'affiche sur l'html dans le paragraphe texte
+    document.getElementById("texte").innerHTML += i ;
+}
+}
+/*on prend l'élément bouton et on lui donne l'évenement click qui affichera la fonction où
+est contenue la boucle */
+document.getElementById("valider").addEventListener("click", listeChiffre);
