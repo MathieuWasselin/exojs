b913a71366609d362f3a5934a5562d0ffe15ebb0
MathieuWasselin <wasselinmathieu@gmail.com>
2021-11-04 11:54:45 +0100

    changement

---
+++ style.js
@@ -0,0 +1,13 @@
+
+function listechiffre(){
+
+
+const test = 5;
+
+for (let i = 0; i < test; i++) {
+    console.log(i); // 0, 1, 2, 3, 4
+}
+
+document.getElementById("p").addEventListener("click");
+
+ }
